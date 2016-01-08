6c34f4b8a2e0620ba3bd821dd5d6bde64c47f885
sh00x <sh00x.dev@gmail.com>
2016-01-08 08:42:48 +0100

    Small code upgrade

--- src/com/diff/Main.java
+++ src/com/diff/Main.java
@@ -43,25 +43,26 @@ public class Main {
         int size1 = list1.size();
         int size2 = list2.size();
 
+        String prod;
         for (int i = 0; i < size1; i++) {
-            String test = list1.get(i);
-            if (list1.contains(test) && list2.contains(test)) {
-                addSet.add(test);
-            }
+            prod = list1.get(i);
+            if (list1.contains(prod) && list2.contains(prod))
+                addSet.add(prod);
         }
 
         for (int i = 0; i < size2; i++) {
-            String test = list2.get(i);
-            if (list1.contains(test) && list2.contains(test)) {
-                addSet.add(test);
-            }
+            prod = list2.get(i);
+            if (list1.contains(prod) && list2.contains(prod))
+                addSet.add(prod);
         }
 
         printlnSave("Różnice w plikach:", writer);
         if (size1 >= size2) {
+            String line1;
+            String line2;
             for (int i = 0; i < size2; i++) {
-                String line1 = list1.get(i);
-                String line2 = list2.get(i);
+                line1 = list1.get(i);
+                line2 = list2.get(i);
                 if (!(list1.contains(line2))) {
                     printlnSave("P1/P2 l." + (i + 1) + "/l." + (i + 1) + ": ", writer);
                     stringDiff(line1, line2, writer);
@@ -72,9 +73,11 @@ public class Main {
                 }
             }
         } else {
+            String line1;
+            String line2;
             for (int i = 0; i < size1; i++) {
-                String line1 = list1.get(i);
-                String line2 = list2.get(i);
+                line1 = list1.get(i);
+                line2 = list2.get(i);
                 if (!(list2.contains(line1))) {
                     printSave("P1/P2 l." + (i + 1) + "/l." + (i + 1) + ": ", writer);
                     stringDiff(line1, line2, writer);
@@ -89,31 +92,26 @@ public class Main {
         printlnSave("\nDodatkowe linie:", writer);
         if (size1 <= size2) {
             for (int i = 0; i < size2; i++) {
-                if (!(addSet.contains(list2.get(i)))) {
+                if (!(addSet.contains(list2.get(i))))
                     printlnSave("P2 l." + (i + 1) + ": " + list2.get(i), writer);
-                }
             }
         } else {
             for (int i = 0; i < size1; i++) {
-                if (!(addSet.contains(list1.get(i)))) {
+                if (!(addSet.contains(list1.get(i))))
                     printlnSave("P1 l." + (i + 1) + ": " + list1.get(i), writer);
-                }
             }
         }
         writer.close();
     }
 
-    public static void stringDiff(String in1, String in2, PrintWriter writer) throws IOException {
+    private static void stringDiff(String in1, String in2, PrintWriter writer) throws IOException {
         String[] array1 = in1.split(" ");
         String[] array2 = in2.split(" ");
 
         for (int i = 0; i < array1.length; i++) {
             try {
-                if (array1[i].equals(array2[i])) {
-                    printSave(array1[i] + " ", writer);
-                } else {
-                    printSave("\"" + array1[i] + "\" / \"" + array2[i] + "\" ", writer);
-                }
+                if (array1[i].equals(array2[i])) printSave(array1[i] + " ", writer);
+                else printSave("\"" + array1[i] + "\" / \"" + array2[i] + "\" ", writer);
             } catch (ArrayIndexOutOfBoundsException ignored) {
             }
         }
